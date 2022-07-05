3fba3572006b601844063a9557429b266f1ff81f
Benjamin Heath <benjaminheath238@gmail.com>
2022-07-05 07:28:24 +0100

    Added striped and industrial blocks and items

--- src/main/java/com/bjmh/silitics/block/ModBlocks.java
+++ src/main/java/com/bjmh/silitics/block/ModBlocks.java
@@ -6,9 +6,86 @@ import java.util.List;
 import com.bjmh.silitics.factory.BlockFactory;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 
 public enum ModBlocks {
-    ;
+    STRIPES() {
+        @Override
+        public void generate(BlockFactory factory) {
+            factory.newBlockGroup("stripes", 2.5F, 10F, 0F, Material.IRON, false)
+            .next("black_green")
+            .next("black_orange")
+            .next("black_white")
+            .next("black_yellow")
+            .next("green_white")
+            .next("red_white")
+            .next("white_blue")
+            .next("black_green_plate")
+            .next("black_orange_plate")
+            .next("black_white_plate")
+            .next("black_yellow_plate")
+            .next("green_white_plate")
+            .next("red_white_plate")
+            .next("white_blue_plate")
+            .build();
+        }
+    },
+    
+    INDUSTRIAL_CLEAN() {
+        @Override
+        public void generate(BlockFactory factory) {
+            factory.newBlockGroup("industrial_clean", 4F, 15F, 0F, Material.IRON, false)
+            .next()
+            .next("plate_large")
+            .next("panel_large")
+            .next("bracing_p4")
+            .next("bracing_p8")
+            .next("ring_p8")
+            .build();
+        }
+    },
+    
+    INDUSTRIAL_SHINY() {
+        @Override
+        public void generate(BlockFactory factory) {
+            factory.newBlockGroup("industrial_shiny", 4F, 18F, 0F, Material.IRON, false)
+            .next()
+            .next("plate_large")
+            .next("panel_large")
+            .next("bracing_p4")
+            .next("bracing_p8")
+            .next("ring_p8")
+            .build();
+        }
+    },
+    
+    INDUSTRIAL_NORMAL() {
+        @Override
+        public void generate(BlockFactory factory) {
+            factory.newBlockGroup("industrial_normal", 4F, 15F, 0F, Material.IRON, false)
+            .next()
+            .next("plate_large")
+            .next("panel_large")
+            .next("bracing_p4")
+            .next("bracing_p8")
+            .next("ring_p8")
+            .build();
+        }
+    },
+    
+    INDUSTRIAL_CORRODED() {
+        @Override
+        public void generate(BlockFactory factory) {
+            factory.newBlockGroup("industrial_corroded", 15F, 8F, 0F, Material.IRON, false)
+            .next()
+            .next("plate_large")
+            .next("panel_large")
+            .next("bracing_p4")
+            .next("bracing_p8")
+            .next("ring_p8")
+            .build();
+        }
+    };
 
     public static final List<Block> BLOCKS = new ArrayList<>();
 
--- src/main/java/com/bjmh/silitics/item/ModItems.java
+++ src/main/java/com/bjmh/silitics/item/ModItems.java
@@ -8,7 +8,24 @@ import com.bjmh.silitics.factory.ItemFactory;
 import net.minecraft.item.Item;
 
 public enum ModItems {
-    ;
+    PLATES() {
+        @Override
+        public void generate(ItemFactory factory) {
+            factory.newItemGroup("plate")
+            .next("industrial_clean")
+            .next("industrial_corroded")
+            .next("industrial_normal")
+            .next("industrial_shiny")
+            .next("stripes_black_green")
+            .next("stripes_black_orange")
+            .next("stripes_black_white")
+            .next("stripes_black_yellow")
+            .next("stripes_green_white")
+            .next("stripes_red_white")
+            .next("stripes_white_blue")
+            .build();
+        }
+    };
 
     public static final List<Item> ITEMS = new ArrayList<>();
 
