ae619536be75ea761f1331d2b8fda1438f4f36b4
Benjamin Heath <benjaminheath238@gmail.com>
2022-07-11 10:49:18 +0100

    v1.0.0

--- src/main/java/com/bjmh/silitics/Silitics.java
+++ src/main/java/com/bjmh/silitics/Silitics.java
@@ -20,7 +20,7 @@ import com.bjmh.silitics.world.SiliticsWorldGen;
 public class Silitics {
     public static final String MODID = "silitics";
     public static final String NAME = "Silitics";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.0.0";
 
     public static final Logger LOGGER = LogManager.getLogger(NAME);
     public static final Random RANDOM = new Random();
